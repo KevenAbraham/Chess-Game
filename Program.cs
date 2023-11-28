8fde4cb0407e75a793bdddb62a091dc41b3a4085
KevenAbraham <abraham.keven20@gmail.com>
2023-11-28 01:12:47 -0300

    putting pieces on the board

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace Chess
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
             Tela.ImprimirTabuleiro(tab);
         }
     }
