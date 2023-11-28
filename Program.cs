59f52e772d1a4df8a422caae56f8186c21d0eb86
KevenAbraham <abraham.keven20@gmail.com>
2023-11-28 09:02:32 -0300

    initializng movimentation tests

--- Program.cs
+++ Program.cs
@@ -11,7 +11,22 @@ namespace Chess
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                Tela.ImprimirTabuleiro(partida.Tab);
+                while(!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.Tab);
+
+                    Console.WriteLine();
+
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
+
+                    partida.ExecutarMovimento(origem, destino);
+                }
+
             }
             catch (TabuleiroException e)
             {
