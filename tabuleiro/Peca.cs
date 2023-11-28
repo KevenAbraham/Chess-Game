e86b4be1f9549983fa05c3adb8c94be333a58370
KevenAbraham <abraham.keven20@gmail.com>
2023-11-28 09:13:45 -0300

    adding the possivle moviments for the KIIIIIING

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
@@ -20,6 +20,11 @@ namespace tabuleiro
         public void IncrementarQteMovimentos()
         {
             QteMovimentos++;
+        }
+
+        public abstract bool[,] MovimentosPossiveis() 
+        {
+            
         }
 
     }
